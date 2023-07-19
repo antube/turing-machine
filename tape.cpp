fe7e7bc127f6fca1a31b794d07a55af75b851a24
Andrew Brown <andrew@mkbrown.org>
2023-07-18 22:28:58 -0400

    Comments
    Added some description to help myself (and maybe others) later.

--- tape.cpp
+++ tape.cpp
@@ -5,6 +5,7 @@
 
 Tape::Tape()
 {
+    // make sure the vector has at least a single value in it
     tape.push_back(0);
 }
 
@@ -19,7 +20,9 @@ Tape::~Tape()
 
 uint32_t Tape::read()
 {
-    return tape[tapePosition] & bitMask;
+    //   use the bit mask to mask the bit I want to return and then shift to the right
+    // until it is either a one or a zero (and not by shifting a value of one off the integer)
+    return (tape[tapePosition] & bitMask) >> bitPosition;
 }
 
 
@@ -41,41 +44,58 @@ void Tape::moveHead(move mv)
     // Check if the caller wants to move right
     if (mv == right)
     {
+        //   check if the bitmask is NOT at the far right
         if (bitMask != 0x00000001)
         {
+            // if so shift the bitmask one to the right and subtract 1 from the bit position
             bitMask = bitMask >> 1;
             bitPosition--;
         }
+        // otherwise I need to setup for moving into a different integer
         else
         {
+            // reset the bitmask and bitposition to the needed place in the next integer
             bitMask = 0x80000000;
+            bitPosition = 31;
 
+            // if I am at the final value in the vector add a value to the end of the vector
             if (tapePosition == tape.size() - 1)
                 tape.insert(tape.end(), 0);
-            else
-                tapePosition++;
-
-            bitPosition = 31;
+            
+            // either way I will need to add one to get to the next value in the vector
+            tapePosition++;
         }
     }
     // check if the caller wants to move left
     else if (mv == left)
     {
+        //   if the bit mask is NOT at the far left end of the 32 bit int
+        // shift the mask bit to the left by 1 and add one to the bit position
         if (bitMask != 0x80000000)
         {
             bitMask = bitMask << 1;
             bitPosition++;
         }
+        //   otherwise I have reached the end of the integer and it is time to move to the next integer
         else
         {
+            // reset the bit mask to the base bitmask
             bitMask = 0x00000001;
+            bitPosition = 0;
+
+            //   if the current tape position is at the beginning of the vector I will
+            // need to add a value to the front of the vector
             if (tapePosition == 0)
                 tape.insert(tape.begin(), 0);
+            // otherwise I can continue to move to the left
             else
                 tapePosition--;
-
-            bitPosition = 0;
         }
+    }
+    // just to cover all my bases
+    else if (mv == nothing)
+    {
+        return;
     }
     // if they don't want to do either something is funky with the move enum
     else
--- tape.h
+++ tape.h
@@ -7,23 +7,26 @@
 enum move
 {
     left,
-    right
+    right,
+    nothing
 };
 
 class Tape
 {
 private:
+    // the mask for the bit to read from the current integer in the vector
     uint32_t bitMask = 1;
-    int bitPosition = 0;
+    int bitPosition = 0; // keep track of where the bitmask is currently so I can perform some operation easier
     
-    unsigned long long tapePosition = 0;
+    // tape and value to track the current position
     std::vector<uint32_t> tape;
+    unsigned long long tapePosition = 0;
 
 public:
     Tape();
     ~Tape();
 
-    uint32_t read();
-    void write(uint32_t);
-    void moveHead(move);
+    uint32_t read();      // read current value from tape
+    void write(uint32_t); // write a value to the current position
+    void moveHead(move);  // move the read/write head by 1 left or right
 };
